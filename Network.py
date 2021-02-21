5d1b0f06d8e5b3f63c2897935bb93b373d9ff1d5
Markus Ullenbruch <66627483+MarkusUllenbruch@users.noreply.github.com>
2021-02-21 19:21:23 +0100

    First

--- Network.py
+++ Network.py
@@ -54,19 +54,46 @@ class Network:
             output = layer.forward(output)
         return output
 
-    def fit(self, X, y, epochs, mini_batch_size, learning_rate):
-        pass
+    def fit(self, X, y, epochs, mini_batch_size, learning_rate, shuffle=True):
+        print('Start Training!')
+
+        # Shuffle all training Data
+        if shuffle == True:
+            X = X
+            y = y
+
+        # Split whole Batch in Mini-Batches
+        mini_batches_X = []
+        mini_batches_y = []
+        for i in range(X.shape[0]//mini_batch_size):
+            print(i)
+            X_mini_batch = X[i*mini_batch_size:(i+1)*mini_batch_size, :]
+            y_mini_batch = y[i*mini_batch_size:(i+1)*mini_batch_size, :]
+            mini_batches_X.append(X_mini_batch)
+            mini_batches_y.append(y_mini_batch)
+
+        # Training Loop
+        for epoch in range(epochs):
+            for XX, yy in zip(mini_batches_X, mini_batches_y):
+                pass
+
+
+
+            print('Epoch ' + str(epoch+1) + '/' + str(epochs))
 
     def backprop(self, x, y):
         pass
 
 
 model = Network()
-model.add(Dense(100, 50, activation=Linear()))
-model.add(Dense(50, 50, activation=Linear()))
-model.add(Dense(50, 2, activation=Linear()))
+model.add(Dense(2, 10, activation=Linear()))
+model.add(Dense(10, 1, activation=Linear()))
 
 
-batch = np.random.randint(low=-2, high=2, size=(5,100))
-Y = model.predict(batch)
-print(Y.shape)
+X_train = np.array([[0, 0], [0, 1], [1, 0], [1, 1]])
+y_train = np.array([[0], [0], [0], [1]])
+
+Y = model.predict(X_train)
+print(Y.shape, Y)
+
+model.fit(X_train, y_train, epochs=10, mini_batch_size=2, learning_rate=0.001)
