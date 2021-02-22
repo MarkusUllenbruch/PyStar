15c08beaa314d9e39752c8a960a7ff57c2368576
Markus Ullenbruch <66627483+MarkusUllenbruch@users.noreply.github.com>
2021-02-22 09:54:41 +0100

    New MSE Class

--- Network.py
+++ Network.py
@@ -23,11 +23,22 @@ class Linear:
     def derivative(self):
         return 1.0
 
+class MSE:
+    def __init__(self):
+        pass
+
+    def __call__(self, y_true, y_pred):
+        return np.mean(np.square(y_true - y_pred))
+
+    def derivative(self, y_true, y_pred):
+        return 2*np.mean(y_true - y_pred)
+
 class Dense(Layer):
     def __init__(self, input_neurons, output_neurons, activation):
         super().__init__()
         self.activation = activation
         self.weights = np.random.rand(input_neurons, output_neurons) - 0.5
+        #print('WEIGHTS  ',self.weights.shape)
         self.bias = np.random.rand(1, output_neurons) - 0.5
 
     def forward(self, inputs):
@@ -39,10 +50,11 @@ class Dense(Layer):
 
 
 class Network:
-    def __init__(self):
+    def __init__(self, loss):
         self.layers = []
         self.loss = None
         self.loss_prime = None
+        self.loss = loss
 
     # Adding layer object to list self.layers
     def add(self, layer):
@@ -74,8 +86,24 @@ class Network:
 
         # Training Loop
         for epoch in range(epochs):
-            for XX, yy in zip(mini_batches_X, mini_batches_y):
-                pass
+            for XX, y_true in zip(mini_batches_X, mini_batches_y):
+
+                # Forward Pass
+                y_pred = self.predict(XX)
+
+                # Backward Pass
+                errors = []
+                delta = self.loss(y_true, y_pred) * self.layers[-1].z
+                errors.append(delta)
+
+                print('LAYERS:', len(self.layers))
+
+                for i in range(len(self.layers)-2, -1, -1):
+                    print(i)
+                    delta = np.dot(self.layers[i+1].weights.transpose(), delta) * self.layers[i].activation(self.layers[i].z)
+                    errors.append(delta)
+
+
 
 
 
@@ -85,13 +113,15 @@ class Network:
         pass
 
 
-model = Network()
+model = Network(loss=MSE())
 model.add(Dense(2, 10, activation=Linear()))
+model.add(Dense(10, 10, activation=Linear()))
+model.add(Dense(10, 10, activation=Linear()))
 model.add(Dense(10, 1, activation=Linear()))
 
 
 X_train = np.array([[0, 0], [0, 1], [1, 0], [1, 1]])
-y_train = np.array([[0], [0], [0], [1]])
+y_train = np.array([[0, 0], [0, 0], [0, 0], [1, 0]])
 
 Y = model.predict(X_train)
 print(Y.shape, Y)
